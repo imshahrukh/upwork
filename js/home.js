3d5a0ad1d7f14d2499057f9025696c1736511f5f
Shahrukh170C <sk902224@gmail.com>
2021-08-10 22:32:26 +0500

    push

--- js/home.js
+++ js/home.js
@@ -1,8 +1,14 @@
 const cross = document.getElementsByClassName("cross")[0];
-const links = document.getElementsByClassName('nav-item')[0]
+const links = document.getElementsByClassName("nav-item")[0];
 
+const nav = document.getElementsByClassName("lst-child");
+console.log(nav);
+cross.addEventListener("click", (e) => {
+  links.classList.toggle("show");
+});
 
-cross.addEventListener('click',(e)=>{
-    links.classList.toggle("show");
-
-})
+Array.from(nav).map((el) => {
+  el.addEventListener("click", () => {
+    console.log(el.classList.toggle("brd"));
+  });
+});
